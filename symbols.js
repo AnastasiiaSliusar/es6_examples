e0ff883a0ceb56e3fdf7e909ffce63e21725aa4d
AnastasiiaSliusar <mokaS32@yandex.ru>
2015-09-17 09:57:20 +0300

    Update symbols.js

--- symbols.js
+++ symbols.js
@@ -1,6 +1,6 @@
 
  'use strict';
-/* var sym1 = Symbol(),
+ var sym1 = Symbol(),
   sym2 = Symbol("foo"),//символ з описом
   sym3 = Symbol("foo");
  if(sym2 === sym3){
@@ -9,10 +9,8 @@
      console.log('no');
  }
 
- */
-
 //new Symbol() - буде помилка
-/*
+
  var sym1 = Symbol(),
    sym2 = Symbol("foo"),
    sym3 = Symbol("foo"),
@@ -24,37 +22,25 @@
   }
  console.log('typeof_sym2-->',(typeof sym2));
  console.log('typeof_object1-->',(typeof object1));
- */
  
- 
-//Символи в циклі for in не піддаються ітерації
-/*
+//Символи в циклі for in ігноруються
  var obj = {};
-
  obj[Symbol("a")] = "a";
  obj[Symbol.for("b")] = "b";
  obj["c"] = "c";
  obj.d = "d";
 
  for (var i in obj) {
-    console.log(i); // logs "c" and "d"
+    console.log(i); 
  }
- */
-
-
+ 
 //обчислювальні властивості об'єкта
-
- /*
   const MY_KEY = Symbol();
    let obj = {};
 
    obj[MY_KEY] = 123;
    console.log(obj[MY_KEY]);
-  */
-
-
-
-  /*
+  
    const FOO = Symbol();
      let obj = {
       [FOO](a) {
@@ -64,77 +50,26 @@
      },
       b=1234;
      console.log(obj[FOO](b)); 
-   */
+  
 
 /*
 Property keys - string or symbols
 String-value keys - property name
 Symbol-value keys - symbol property
 */
-  /*
+  
    let obj = {
       [Symbol('my_key')]: 1,
       enum: 2,
       nonEnum: 3
      };
-   */
-  //console.log(Object.defineProperty(obj,'nonEnum', { enumerable: false }));
+   
+  console.log(Object.defineProperty(obj,'nonEnum', { enumerable: false }));
   //console.log(Object.getOwnPropertyNames(obj));
- //console.log(Object.getOwnPropertySymbols(obj));
-//console.log(Object.keys(obj));
-
-
-
-
-
- //перелік ключів
- /*
- var COLOR_RED    = 'RED';
- var COLOR_ORANGE = 'ORANGE';
- var COLOR_YELLOW = 'YELLOW';
- var COLOR_GREEN  = 'GREEN';
- var COLOR_BLUE   = 'BLUE';
- var COLOR_VIOLET = 'VIOLET';
-
+  //console.log(Object.getOwnPropertySymbols(obj));
+  //console.log(Object.keys(obj));
  
- const COLOR_RED    = Symbol();
- const COLOR_ORANGE = Symbol();
- const COLOR_YELLOW = Symbol();
- const COLOR_GREEN  = Symbol();
- const COLOR_BLUE   = Symbol();
- const COLOR_VIOLET = Symbol();
-
-
- function getComplement(color) {
-    console.log('color-->',color);
-  switch (color) {
-   case COLOR_RED:
-    return COLOR_GREEN;
-   case COLOR_ORANGE:
-    return COLOR_BLUE;
-   case COLOR_YELLOW:
-    return COLOR_VIOLET;
-   case COLOR_GREEN:
-    return COLOR_RED;
-   case COLOR_BLUE:
-    return COLOR_ORANGE;
-   case COLOR_VIOLET:
-    return COLOR_YELLOW;
-   default:
-    return ('Unknown color: '+color);
-  }
- }
-
-var MOOD_BLUE = 'BLUE',
-result=getComplement(MOOD_BLUE);
-console.log('result-->',result);
-
-*/
- 
-
-
  //iterability
-/*
   let obj = {
    data: [ 'hello', 'world' ],
    [Symbol.iterator]() {
@@ -157,20 +92,13 @@ console.log('result-->',result);
  for (let item of obj ){
      console.log('item-->',item);
  }
- */
-/*
- //global symbol register
 
+ //global symbol register
  let sym = Symbol.for('Hello everybody!');
- console.log('sym-->'
- )
- 
-  
-  Symbol.keyFor(sym);
+ console.log('sym-->',sym);
+ console.log(Symbol.keyFor(sym));
 
  //Property access via [ ]
-
-
  const sym = Symbol('yes');
  let obj = {
   [sym]: 'a',
@@ -178,29 +106,7 @@ console.log('result-->',result);
  };
  let wrappedSymbol = Object(sym);
  console.log(typeof wrappedSymbol);
-
-
  let wrappedString = new String('str');
-  console.log(typeof wrappedString);
-
-  console.log(obj[wrappedString]);
-
-*/
- //JSON.stringify
-
-
- /*
-  function symbolReplacer(key, value) {
-    if (typeof value === 'symbol') {
-     return '@@' + Symbol.keyFor(value) + '@@';
-    }
-    return value;
-   }
-   const MY_SYMBOL = Symbol.for('http://example.com/my_symbol');
-   let obj = { [MY_SYMBOL]: 123 };
-
-   let str = JSON.stringify(obj, symbolReplacer);
-   console.log(str);
-  */
-
+ console.log(typeof wrappedString);
+ 
 
